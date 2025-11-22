28316c7eed4cbb09889dd4bfdfd3aefcd3c5fec2
SAGNIK DHARA MEGH <sagnikdhara20@gmail.com>
2025-11-22 08:38:36 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-3595146144-18ce7",
+  "appId": "1:658966217896:web:f34c00bbaca386b1a0ee9d",
+  "apiKey": "AIzaSyD85P11nJF6E1Ky6fI8tSv4WXY8sQrMTIk",
+  "authDomain": "studio-3595146144-18ce7.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "658966217896"
+};
